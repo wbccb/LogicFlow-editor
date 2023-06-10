db918fa672236801d29a92b9b82663cdf11133ab
wbccb <wbccb18814122@163.com>
2023-06-10 16:57:13 +0800

    feat: LogicFlow增加拖动画布

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import {createApp} from "vue";
 import "./styles/index.scss";
+import "@logicflow/core/dist/style/index.css";
+import "@logicflow/extension/lib/style/index.css";
 import App from "./App.vue";
 import router from "./router";
 import ElementPlus from "element-plus";
